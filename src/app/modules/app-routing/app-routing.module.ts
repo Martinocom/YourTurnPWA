efafc00cb35c72803e23828bf5d62ef38e8779c6
Marcin Pabich <martinoqom@gmail.com>
2021-09-16 15:49:26 +0200

    Trying to enable first angular navigation and login

---
+++ src/app/modules/app-routing/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// Import AuthGuard of Firestore
+import { AngularFireAuthGuard, redirectUnauthorizedTo} from '@angular/fire/compat/auth-guard';
+import { LandingComponent } from 'src/app/components/landing/landing.component';
+
+
+// Import all the components for which navigation service has to be activated 
+import { DashboardComponent } from '../../components/dashboard/dashboard.component';
+import { ForgotPasswordComponent } from '../../components/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from '../../components/verify-email/verify-email.component';
+import { LoginComponent } from 'src/app/components/login/login.component';
+import { RegisterComponent } from 'src/app/components/register/register.component';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+
+const routes: Routes = [
+  { path: '', redirectTo: 'landing'},
+  { path: 'landing', component: LandingComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register-user', component: RegisterComponent},
+  { path: 'forgot-password', component: ForgotPasswordComponent},
+  { path: 'verify-email-address', component: VerifyEmailComponent},
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin }},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, NgZone } from '@angular/core';
+
+import { User } from "../model/user"
+import { Router } from "@angular/router";
+
+
+
+// Firestore
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthService {
+  
+
+}
