d06381ce5170da75663a919e10e055c952bf9ddc
Marcin Pabich <martinoqom@gmail.com>
2021-09-18 17:35:02 +0200

    Fixing reference to PWA

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/YourTurnPWA'),
+      dir: require('path').join(__dirname, './coverage/YourTurn'),
       subdir: '.',
       reporters: [
         { type: 'html' },
